df8f51e86f66e8567f82a391e24bf0c2d2335ebd
PhMota <philipe.mota@gmail.com>
2021-07-13 18:47:42 -0300

    load python3.6

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 screen -dRRS connie-skipper bash -c "
 echo 'screen started.'
 cd ~/connie-skipper
+module load py-notebook/6.1.5-gcc-8.3.0-lim66g5 softwares/texlive/2017 python/3.6.3-gcc-5.3.0
 pip3 --version
 pwd
-module load py-notebook/6.1.5-gcc-8.3.0-lim66g5 softwares/texlive/2017
 echo 'modules loaded:'
 module list
 echo 'update connie-skipper.git'
