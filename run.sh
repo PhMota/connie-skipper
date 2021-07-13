a16ce104cc17becaf24d15853d9f1d51df42fd79
PhMota <philipe.mota@gmail.com>
2021-07-13 18:35:00 -0300

    added voila

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
-screen -dRRS connie-skipper bash -c "\
-cd ~/connie-skipper;\
-git pull;\
-module load py-notebook/6.1.5-gcc-8.3.0-lim66g5 softwares/texlive/2017;\
-pip3 install --upgrade pip3 --user;\
-pip3 install ipywidgets --user;\
-pip3 install xarray --user;\
-jupyter notebook --no-browser --port=8889;\
+screen -dRRS connie-skipper bash -c "
+echo 'screen started.'
+cd ~/connie-skipper
+pip3 --version
+pwd
+module load py-notebook/6.1.5-gcc-8.3.0-lim66g5 softwares/texlive/2017
+echo 'modules loaded:'
+module list
+echo 'update connie-skipper.git'
+git pull
+echo 'update requirements:'
+python --version
+pip3 --version
+pip3 install --upgrade pip3 --user
+pip3 install voila --user
+pip3 install ipywidgets --user
+pip3 install xarray --user
+echo 'start jupyter notebook'
+voila skipper.ipynb --no-browser --port=8889
 "
--- runremote.sh
+++ runremote.sh
@@ -1,4 +1,13 @@
 echo "forward che port 8889 to local port 8889"
 ssh -N -f -L localhost:8889:localhost:8889 che.cbpf.br -p 13900
 echo "update remote app and start jupyter at che in port 8889"
-ssh che.cbpf.br -p 13900 "bash ~/connie-skipper/run.sh"
+ssh -t che.cbpf.br -p 13900 "
+source ~/.bashrc
+source ~/.bash_profile
+echo 'screen started.'
+if [ ! -d '~/connie-skipper' ] 
+then
+    git clone https://github.com/PhMota/connie-skipper.git
+fi
+bash ~/connie-skipper/run.sh
+"
