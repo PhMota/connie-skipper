a1043d9758aefd734863f4582765c015172ca76b
PhMota <philipe.mota@gmail.com>
2021-07-13 15:41:08 -0300

    fix ssh remote cmd

--- runremote.sh
+++ runremote.sh
@@ -1,4 +1,4 @@
 echo "forward che port 8889 to local port 8889"
 ssh -N -f -L localhost:8889:localhost:8889 che.cbpf.br -p 13900
 echo "update remote app and start jupyter at che in port 8889"
-ssh che.cbpf.br -p 13900 run.sh
+ssh che.cbpf.br -p 13900 "bash ~/connie-skipper/run.sh"
