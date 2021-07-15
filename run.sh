35305e7a1fc3c3f0958bf817e630af4645791ff6
PhMota <philipe.mota@gmail.com>
2021-07-15 04:29:08 -0300

    run.sh import xarray check

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ echo 'screen started.'
 cd ~/connie-skipper
 echo -e '\e[33mupdate connie-skipper.git\e[0m'
 git pull
-module load py-notebook/6.1.5-gcc-8.3.0-lim66g5 softwares/texlive/2017 python/3.6.3-gcc-5.3.0
+module load py-notebook softwares/texlive python
 if [ ! -x ~/.local/bin/pip3 ]
 then 
     echo -e '\e[33install pip\e[0m'
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python3 get-pip.py --user
 fi
-if [ ! $(python3 -c "import xarray, matplotlib, astropy, ipympl, voila, ipywidgets" 2> /dev/null) ]
+if ! python3 -c 'import xarray'
 then 
     echo -e '\e[33minstall requirements:\e[0m'
     pip3 install --upgrade pip --user
