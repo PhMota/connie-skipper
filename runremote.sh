8e527c8682c4d6fe4cb5fa8cd87011862515e267
PhMota <philipe.mota@gmail.com>
2021-07-15 04:59:43 -0300

    runremote.sh working

--- runremote.sh
+++ runremote.sh
@@ -1,19 +1,27 @@
-echo "update remote app and start jupyter at che in port 8889"
-touch runremote.log
-tail -f runremote.log | grep http://localhost: | grep -o "[0-9]*" -q;\
-PORT = $(tail runremote.log | grep http://localhost: | grep -o "[0-9]*");\
-ssh -N -f -L localhost:$PORT:localhost:$PORT che.cbpf.br -p 13900;\
-echo "app running at (open link in browser)";\
-echo "http://localhost:$PORT/" &
+echo -e "\n" > runremote.log
+echo -e "\e[34mwaiting for the server port...\e[0m"
+bash -c "
+tail -f runremote.log | grep http://localhost -q
+PORT=\$(cat runremote.log | grep http://localhost | grep -o '[0-9]*')
+ssh -N -f -L localhost:\$PORT:localhost:\$PORT che.cbpf.br -p 13900
+echo -e '\\e[33mapp is running locally at the above link (open link in browser)\\e[0m'
+echo -e '\\n'
+echo -e \"\\r\\e[4mhttp://localhost:\$PORT/\\e[0m\"
+" &
 
+echo -e "\e[34mstarting server at che\e[0m"
 ssh -t che.cbpf.br -p 13900 "
 source ~/.bashrc
 source ~/.bash_profile
-echo 'screen started.'
-if [ ! -d '~/connie-skipper' ] 
+echo -e '\e[33mscreen started.\e[0m'
+pwd
+if [ ! -d ~/connie-skipper/ ] 
 then
+    echo 'cloning the repository from https://github.com/PhMota/connie-skipper.git'
     git clone https://github.com/PhMota/connie-skipper.git
 else
+    echo 'updating connie-skipper'
+    cd ~/connie-skipper/
     git pull
 fi
 bash ~/connie-skipper/run.sh
